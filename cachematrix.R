437881f14754099bf22ef623cea0d2bab6dc553e
DarthXeth <xwaxman@gmail.com>
2014-05-20 12:10:04 -0600

    Implemented cache-able matrix formula
    Forked cahcematrix.x to create cache-able matrix so that matrix
    inversion performance can be optomized

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix creates our new cache-able matrix
+## cacheSolve (weird name) will pull the result from the cache if available,
+## or solve if nothing is cached
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  ##create our cached inversed
+  inverseOfX <- NULL
+  
+  ##use the <<- to *set the values in 'the other' environment
+  set <- function(y) {
+    x <<- y
+    inverseOfX <<- NULL
+  }
+  
+  ##create the *get 
+  get <- function() x
+  
+  ##*set the inverse
+  setInverse <- function(inverse) inverseOfX <<- inverse
+  
+  ##*get the inverse
+  getInverse <- function() inverseOfX
+  
+  ##now return a list with these values
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+    
 }
 
 
-## Write a short comment describing this function
+## cacheSolve will take a special cache-able matrix 'x' created with
+## makeCacheMatrix, and return the inverse of 'this 'x'. The inverse
+## of 'x' will be pulled from the cache, if available, rather than solved
+## to increase performance, and to pass the assignment. Mostly the 2nd.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  ## get the cached inverse
+  inverseOfX <- x$getInverse()
+  
+  ##solve if the cahced inverse is null
+  if (is.null(inverseOfX)) {
+    inverseOfX <- solve(x$get())
+    x$setInverse(inverseOfX)
+  } else {
+    message("using cached matrix")
+  }
+  
+  return inverseOfX
 }
